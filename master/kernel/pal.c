75ee041eb637bde9bfcd45dfcf7142a7608d95f3
Sascha Ittner <sascha.ittner@modusoft.de>
2026-02-12 22:24:38 +0100

    fixed kernel module build

--- master/kernel/pal.c
+++ master/kernel/pal.c
@@ -420,10 +420,7 @@ void ec_device_poll(
         ec_device_t *device /**< EtherCAT device */
         )
 {
-#ifdef EC_HAVE_CYCLES
-    device->cycles_poll = get_cycles();
-#endif
-    device->jiffies_poll = jiffies;
+    device->time_poll = ec_current_time();
 #ifdef EC_DEBUG_RING
     do_gettimeofday(&device->timeval_poll);
 #endif
